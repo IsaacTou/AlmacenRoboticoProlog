ee2d9e3993d1a095251b542f63a097f97e61db12
Isaac <isaacpereztouceda@gmail.com>
2026-06-21 20:06:13 -0400

    Finalizando el proyecto

--- Proyecto2.pl
+++ Proyecto2.pl
@@ -16,19 +16,19 @@
 % Parte 1: Inicialización del Tablero 
 % ======================================================================================================
 
-% Predicados dinámicos
+% Predicados dinámicos.
 :- dynamic robot/2.
 :- dynamic caja_objetivo/2.
 :- dynamic caja_bloqueo/2.
 
-% Para evaluar si una entidad se encuentra en los límites del tablero
+% Para evaluar si una entidad se encuentra en los límites del tablero.
 enTablero((Fila, Columna)) :-
     Fila >= 0, 
     Fila =< 5,
     Columna >= 0,
     Columna =< 5. 
 
-% Para verificar que la lista de cajas de bloqueo se encuentre en el tablero y no haya solapamientos
+% Para verificar que la lista de cajas de bloqueo se encuentre en el tablero y no haya solapamientos.
 verificacionLista(_, _, []).
 verificacionLista((FilaRobot, ColumnaRobot), (FilaObjetivo, ColumnaObjetivo), [(FilaObstaculo, ColumnaObstaculo) | T]) :-
     enTablero((FilaObstaculo, ColumnaObstaculo)),
@@ -37,13 +37,13 @@ verificacionLista((FilaRobot, ColumnaRobot), (FilaObjetivo, ColumnaObjetivo), [(
     (FilaObjetivo, ColumnaObjetivo) \= (FilaObstaculo, ColumnaObstaculo),
     verificacionLista((FilaRobot, ColumnaRobot), (FilaObjetivo, ColumnaObjetivo), T).
 
-% Para agregar a la base de conocimientos la lista de cajas de bloqueo si la misma es válida
+% Para agregar a la base de conocimientos la lista de cajas de bloqueo si la misma es válida.
 cargarObstaculos([]).
 cargarObstaculos([(FilaObstaculo, ColumnaObstaculo) | T]) :-
     assertz(caja_bloqueo(FilaObstaculo, ColumnaObstaculo)),
     cargarObstaculos(T).
 
-% Para garantizar el correcto estado inicial de las entidades
+% Para garantizar el correcto estado inicial de las entidades.
 initialBoard((FilaRobot, ColumnaRobot), (FilaObjetivo, ColumnaObjetivo), BlockingBoxes) :- 
     % para garantizar la limpieza de ejecuciones previas
     %retractall(robot(_, _)),
@@ -55,12 +55,12 @@ initialBoard((FilaRobot, ColumnaRobot), (FilaObjetivo, ColumnaObjetivo), Blockin
     (FilaRobot, ColumnaRobot) \= (FilaObjetivo, ColumnaObjetivo),
     verificacionLista((FilaRobot, ColumnaRobot), (FilaObjetivo, ColumnaObjetivo), BlockingBoxes),
 
-    % para garantizar la limpieza de ejecuciones previas
+    % para garantizar la limpieza de ejecuciones previas.
     retractall(robot(_, _)),
     retractall(caja_objetivo(_, _)),
     retractall(caja_bloqueo(_, _)),
 
-    % Si todo es válido se carga la base de conocimientos con los hechos correspondientes
+    % Si todo es válido se carga la base de conocimientos con los hechos correspondientes.
     assertz(robot(FilaRobot, ColumnaRobot)),
     assertz(caja_objetivo(FilaObjetivo, ColumnaObjetivo)),
     cargarObstaculos(BlockingBoxes).
@@ -69,20 +69,20 @@ initialBoard((FilaRobot, ColumnaRobot), (FilaObjetivo, ColumnaObjetivo), Blockin
 % Parte 2: Validación de movimientos 
 % ======================================================================================================
 
-% Definiendo los tipos de movimientos posibles
+% Definiendo los tipos de movimientos posibles.
 posicionNueva((FilaActual, ColumnaActual), 'u', (FilaNueva, ColumnaActual)) :- FilaNueva is FilaActual-1.
 posicionNueva((FilaActual, ColumnaActual), 'd', (FilaNueva, ColumnaActual)) :- FilaNueva is FilaActual+1.
 posicionNueva((FilaActual, ColumnaActual), 'l', (FilaActual, ColumnaNueva)) :- ColumnaNueva is ColumnaActual-1.
 posicionNueva((FilaActual, ColumnaActual), 'r', (FilaActual, ColumnaNueva)) :- ColumnaNueva is ColumnaActual+1.
 
-% Caso 1: El robot puede moverse libremente porque su nueva posición no choca con la caja objetivo, ni con ninguna caja de bloqueo
+% Caso 1: El robot puede moverse libremente porque su nueva posición no choca con la caja objetivo, ni con ninguna caja de bloqueo.
 isValidMove(state(Robot, Objetivo, Obstaculos), Move) :-
     posicionNueva(Robot, Move, NewPosicionRobot),
     enTablero(NewPosicionRobot),
     NewPosicionRobot \= Objetivo,
     \+ member(NewPosicionRobot, Obstaculos).
 
-% Caso 2: La nueva posición del robot choca con la caja objetivo
+% Caso 2: La nueva posición del robot choca con la caja objetivo.
 isValidMove(state(Robot, Objetivo, Obstaculos), Move) :-
     posicionNueva(Robot, Move, NewPosicionRobot),
     enTablero(NewPosicionRobot),
@@ -91,7 +91,7 @@ isValidMove(state(Robot, Objetivo, Obstaculos), Move) :-
     enTablero(NewPosicionObjetivo),
     \+ member(NewPosicionObjetivo, Obstaculos).
 
-% Caso 3: La nueva posición del robot choca con una caja de bloqueo
+% Caso 3: La nueva posición del robot choca con una caja de bloqueo.
 isValidMove(state(Robot, Objetivo, Obstaculos), Move) :-
     posicionNueva(Robot, Move, NewPosicionRobot),
     enTablero(NewPosicionRobot),
@@ -102,52 +102,74 @@ isValidMove(state(Robot, Objetivo, Obstaculos), Move) :-
     \+ member(NewPosicionObstaculo, Obstaculos),
     NewPosicionObstaculo \= Objetivo.
 
+% =====================================================================================================
+% Parte 3: Ejecución de movimiento
+% =====================================================================================================
 
-
-% Seccion 3
-
+% Predicado para actualizar la lista de cajas de bloqueo con las nuevas posiciones de las cajas de bloqueo 
+% después de ejecutar un movimiento, si es que el movimiento implica empujar una caja de bloqueo.
 modificarObstaculos(ObstaculoSinModificar, ObstaculoModificado, Obstaculos, [ObstaculoModificado | ObstaculosModificados]) :-
     select(ObstaculoSinModificar, Obstaculos, ObstaculosModificados).
 
+% Caso 1: el robot se mueve a una casilla libre.
 moveRobot(state(Robot, Objetivo, Obstaculos), Move, state(NewRobot, Objetivo, Obstaculos)) :-
+    isValidMove(state(Robot, Objetivo, Obstaculos), Move),
     posicionNueva(Robot, Move, NewRobot),
-    enTablero(NewRobot),
     NewRobot \= Objetivo,
     \+ member(NewRobot, Obstaculos).
 
-moveRobot(state(Robot,Objetivo,Obstaculos), Move, state(NewRobot, NewObjetivo, Obstaculos)) :-
+% Caso 2: el robot se mueve a la posicion donde se encontraba la caja objetivo.
+moveRobot(state(Robot, Objetivo, Obstaculos), Move, state(NewRobot, NewObjetivo, Obstaculos)) :-
+    isValidMove(state(Robot, Objetivo, Obstaculos), Move),
     posicionNueva(Robot, Move, NewRobot),
-    enTablero(NewRobot),
-    NewRobot == Objetivo,
-    posicionNueva(Objetivo, Move, NewObjetivo),
-    enTablero(NewObjetivo),
-    \+ member(NewObjetivo, Obstaculos).
+    NewRobot == Objetivo, 
+    posicionNueva(Objetivo, Move, NewObjetivo).
 
+% Caso 3: el robot se mueve a la posición donde se encontraba una caja de bloqueo.
 moveRobot(state(Robot,Objetivo,Obstaculos), Move, state(NewRobot, Objetivo, NewObstaculos)) :-
+    isValidMove(state(Robot, Objetivo, Obstaculos), Move),
     posicionNueva(Robot, Move, NewRobot),
-    enTablero(NewRobot),
-    member(NewRobot, Obstaculos),
     posicionNueva(NewRobot, Move, NewObstaculo),
-    enTablero(NewObstaculo),
-    \+ member(NewObstaculo, Obstaculos),
     NewObstaculo \= Objetivo,
     modificarObstaculos(NewRobot, NewObstaculo, Obstaculos, NewObstaculos).
 
+% =====================================================================================================
+%Parte 4: Solución 
+% =====================================================================================================
 
-%Parte 4
-
-expandir(nodo(Estado, Camino), HijosValidos) :-
+% Para buscar los hijos de cada nodo.
+expandir(nodo(Estado, Camino), Visitados, HijosValidos) :-
     findall(
         nodo(NuevoEstado, NuevoCamino),
         (
-            moveRobot(Estado, Move, NuevoEstado),
-            append(Camino, [Move], NuevoCamino)   
+            moveRobot(Estado, Move, NuevoEstado), % evaluamos si se puede realizar el movimiento.
+            \+ member(NuevoEstado, Visitados),    % verificamos que el estado que estoy consultando no
+                                                  % esté ya en la lista de visitados, porque si ya está no lo agregamos.
+            append(Camino, [Move], NuevoCamino)   % Si efectivamente es un estado nuevo, actualizo el 
+                                                  % camino del mismo agregándole el movimiento que puede ejecutar a partir de allí.
         ),
-        HijosValidos
+        HijosValidos % se generan todos los hijos que el estado actual tiene.
     ).
 
-esEstadoFinal(state(Robot,(FilaObjetivo, ColumnaObjetivo),Obstaculos)) :-
-    FilaObjetivo == 5,
-    FilaColummna == 5.
+% Verifica si ya la caja objetivo llegó al estado final.
+esEstadoFinal(state(_, (5, 5), _)).
 
+% Caso base: Si el estado actual en el que estoy es un estado final, se acabó el backtracking y la solución
+% es el camino que haya recorrido ese estado.
+bfs([nodo(EstadoActual, CaminoActual) | _], _, SolucionFinal) :-
+    esEstadoFinal(EstadoActual),
+    SolucionFinal = CaminoActual,
+    !.
 
+% Caso general: Si la caja objetivo no ha llegado al estado final.
+bfs([nodo(EstadoActual, CaminoActual) | RestoCola], Visitados, SolucionFinal) :-
+    NuevosVisitados = [EstadoActual | Visitados], % agrego el estado actual a la lista de visitados.
+    expandir(nodo(EstadoActual, CaminoActual), NuevosVisitados, HijosValidos), % evalúo si me puedo mover (tengo hijos).
+    append(RestoCola, HijosValidos, NuevaCola), % si tengo hijos entonces los agego a la cola.
+    bfs(NuevaCola, NuevosVisitados, SolucionFinal). % continúa la recursión.
+
+% Devuelme el camino (secuencia de movimientos) más corto desde el estado inicial hasta que la 
+% caja objetivo llega al estado final.
+solveWarehouse(StartState, Solution) :-
+    bfs([nodo(StartState, [])],[],Solution).
+    
---
+++ README.md
@@ -0,0 +1,73 @@
+# Proyecto #2 – Prolog Almacén Robótico Lógico
+
+**Universidad Central de Venezuela**
+**Facultad de Ciencias**
+**Escuela de Computación**
+**Asignatura: Lenguajes de Programación**
+**Semestre I-2026**
+
+**Docentes:**
+
+- Eugenio Scalise
+- José Yvimas
+
+**Integrantes:**
+
+- Isaac Pérez C.I. 31.065.844
+- Carmen Medina C.I. 32.061.534
+
+---
+
+## 1. Representación del Problema
+
+- **El Tablero:** Representado como una matriz lógica de 6x6 con coordenadas desde `(0,0)` hasta `(5,5)`.
+- **Estados (`state/3`):** `state(RobotCoord, TargetCoord, ListaBloqueos)`. El estado del sistema se modeló utilizando dicha estructura. Esta almacena de forma compacta un término para la posición del robot, un término para la posición de la caja objetivo y una lista de términos para las posiciones de los bloqueos. Esta representación permite al predicado `moveRobot/3` recibir la configuración actual del tablero y generar limpiamente el siguiente estado tras un movimiento, facilitando la expansión de nodos en el algoritmo de búsqueda BFS sin destruir los estados anteriores."
+- **Movimientos:** Representados por los átomos `'u'`, `'d'`, `'l'` y `'r'`.
+
+## 2. Hechos y Predicados Definidos
+
+## _(Lista y describe brevemente los predicados principales y auxiliares de tu archivo `Proyecto2.pl`)_
+
+**Predicados Dinámicos:**
+
+- `robot(Row, Col)`: Guarda la posición actual inicial del robot.
+- `caja_objetivo(Row, Col)`: Guarda la posición inicial de la caja objetivo.
+- `caja_bloqueo(Row, Col)`: Guarda las posiciones de los obstáculos.
+
+**Predicados Principales:**
+
+- `initialBoard/3`: Valida los límites y solapamientos, limpiando la base previa y haciendo `assertz` de las nuevas entidades.
+- `isValidMove/2`: Lógica para comprobar choques con paredes, empujes válidos y la prohibición de empuje múltiple.
+- `moveRobot/3`: Transición de estados generando el `NewState`.
+- `solveWarehouse/2`: Ejecuta el algoritmo de búsqueda para encontrar la `Solution`.
+
+**Predicados Auxiliares:**
+
+- `enTablero/1`: Verifica que una coordenada esté dentro de los límites del tablero.
+- `verificacionLista/3`: Comprueba que no haya solapamientos entre el robot, la caja objetivo y los bloqueos.
+- `cargarObstaculos`: Predicado para agregar a la base de conocimientos la lista de cajas de bloqueo si la misma es válida.
+- `posicionNueva`: Calcula la nueva posición del robot o la caja tras un movimiento dado.
+- `modificarObstaculos`: Actualiza la lista de bloqueos si la caja objetivo es empujada a una nueva posición.
+- `expandir`: Genera los nuevos estados a partir del estado actual para cada movimiento posible, verificando su validez.
+- `esEstadoFinal`: Verifica si la caja objetivo ha llegado a la posición `(5,5)`.
+- `bfs`: Implementación del algoritmo de búsqueda en anchura, utilizando una cola para gestionar los estados a explorar y una lista de visitados para evitar ciclos.
+
+**Predicados Incorporados (built-in predicates):**
+
+- `assertz/1`: Para agregar hechos dinámicos a la base de conocimientos.
+- `retractall/1`: Para limpiar la base de conocimientos antes de establecer un nuevo estado inicial.
+- `member/2`: Para verificar la presencia de un elemento en una lista, utilizado en la verificación de bloqueos y estados visitados.
+- `append/3`: Para concatenar listas, utilizado en la generación de nuevas secuencias de movimientos.
+- `select/3`: Para seleccionar un elemento de una lista, utilizado para quitar una caja obstaculo con posicion obsoleta para ser modificada por la nueva posicion de la misma caja despues de ser empujada.
+- `findall/3`: Para generar una lista de todos los estados hijos a partir del estado actual, utilizado en la expansión de nodos en el algoritmo BFS. Ya que este predicado funciona como un generador de soluciones, recibe un patrón de búsqueda, una consulta que genera soluciones y una variable para almacenar la lista de soluciones generadas. Esto permite que el algoritmo BFS explore eficientemente todos los estados alcanzables desde el estado inicial sin necesidad de escribir manualmente la lógica para cada movimiento posible.
+
+## 3. Estrategia Utilizada
+
+- **Algoritmo:** Búsqueda en Anchura (BFS).
+- **Justificación:** Se utilizó BFS porque garantiza encontrar la secuencia _mínima_ de movimientos (el camino más corto) para llevar la Caja Objetivo a `(5,5)`, explorando todos los estados a distancia $n$ antes de pasar a $n+1$. En este caso se exploran primero los movimientos que llevan a estados más cercanos al estado inicial, lo que es crucial para evitar caminos largos e ineficientes en un espacio de búsqueda con obstaculos, manejando una lista de visitados para evitar ciclos y estados repetidos, con una cola que nos permite gestionar eficientemente los estados a explorar, ademaás de que al visitar cada estado se verifica si es estado final, lo que garantiza que la primera solución encontrada sea la óptima.
+
+## 4. Referencias y Herramientas Consultadas
+
+- **Libro - Learn Prolog Now!:**
+  - _Autores:_ Patrick Blackburn, Johan Bos, y Kristina Striegnitz.
+  - _Uso:_ Material de apoyo consultado para repasar la sintaxis de los predicados incorporados, la estructura de los árboles de búsqueda y el uso del corte lógico (`!`) en la optimización de predicados.
